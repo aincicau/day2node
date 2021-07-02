37d43f3b983788385854976036870e8c08a82345
aincicau <alexandru.incicau@gmail.com>
2021-07-02 16:44:08 +0300

    cerinte rezolvate

--- express.js
+++ express.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
 
+var mathmodule = require('./mathmodule.js');
+
 //default endpoint
 app.get('/', (req, res) => {
     res.send('Welcome to Atoss World');
-})
+});
+
+app.get('/ex1', (req, res) => {
+    res.send('you entered "ex1" ');
+});
+
+app.get('/ex2', (req, res) => {
+    res.send(mathmodule.add(12,3).toString());
+});
 
 app.listen(3000,() => {
     console.log("server is working");
--- firstjs.js
+++ firstjs.js
@@ -4,14 +4,20 @@ var http = require('http');
 var fs = require('fs');
 
 var ourmodule = require('./ourmodule.js');
+var mathmodule = require('./mathmodule.js');
 
 var server = http.createServer((req, res) => {
-   res.end('Welcome to Atoss World');
+   fs.readFile('index.html', (err, data) => {
+       //res.writeHead(200, {'Content-Type': 'text/html'});
+       res.write(data);
+       return res.end();
+   });
 });
 
 server.listen(3000);
 
 console.log(ourmodule.ourFunction());
+
 fs.readFile('dummy.txt', 'utf-8', (err,data) => {
     if(err){
         console.log('error reading file');
@@ -19,3 +25,9 @@ fs.readFile('dummy.txt', 'utf-8', (err,data) => {
         console.log(data)
     }
 });
+
+console.log(mathmodule.add(2,3));
+console.log(mathmodule.sub(5,2));
+console.log(mathmodule.mul(5,5));
+console.log(mathmodule.div(24,3));
+
